71a536e9c778c4376f24f75271728759667fafe8
lizcanojair <lizcanojair@gmail.com>
2014-05-30 19:10:53 -0500

    Corrección del buscador
    Pendiente de validación.

--- app/controllers/BuscadorController.php
+++ app/controllers/BuscadorController.php
@@ -9,28 +9,56 @@ class BuscadorController  extends BaseController {
 
 
 
-		$producto = DB::table('producto as p')->join('almacen as a','a.producto','=','p.id')
-		->join('sedes as s','a.sede','=','s.id')
-		->join('categorias as c','p.categoria','=','c.id')
-		->join('subcategorias as sc','sc.categoria_id','=','sc.id')
-		->select('a.precio_detal',
-				'a.cantidad',
-				'c.nombre AS categoria_nombre',
-				'p.nombre AS producto_nombre',
-				'p.imagen',
-				'p.id',
-				'p.descripcion AS producto_descripcion',
-				's.nombre_publico AS nombre_sede',
-				's.direccion',
-				's.telefono',
-				'sc.nombre_sub'
-			)
-		->where('p.nombre', 'LIKE', '%' . $keyword . '%')
-		->orWhere('p.descripcion','LIKE', '%' . $keyword . '%' )
-		->orWhere('c.nombre','LIKE', '%' . $keyword . '%' )
-		->orWhere('sc.nombre_sub','LIKE', '%' . $keyword . '%' )
+		// $producto = DB::table('producto as p')->join('almacen as a','a.producto','=','p.id')
+		// ->join('sedes as s','a.sede','=','s.id')
+		// ->join('categorias as c','p.categoria','=','c.id')
+		// ->join('subcategorias as sc','sc.categoria_id','=','sc.id')
+		// ->select('a.precio_detal',
+				// 'a.cantidad',
+				// 'c.nombre AS categoria_nombre',
+				// 'p.nombre AS producto_nombre',
+				// 'p.imagen',
+				// 'p.id',
+				// 'p.descripcion AS producto_descripcion',
+				// 's.nombre_publico AS nombre_sede',
+				// 's.direccion',
+				// 's.telefono',
+				// 'sc.nombre_sub'
+			// )
+		// ->where('p.nmbre', 'REGEXP', '[[:<:]]'.$keyword.'*')
+		// ->orWhere('p.descripcion','REGEXP', "[[:<:]]".$keyword."*")
+		// ->orWhere('c.nombre','REGEXP', "[[:<:]]".$keyword."*")
+		// ->orWhere('sc.nombre_sub','REGEXP', "[[:<:]]".$keyword."*")
 
-		->get();
+		// ->get();
+		
+		$producto = DB::select(DB::raw("SELECT 
+  a.precio_detal,
+  a.cantidad,
+  c.nombre AS categoria_nombre,
+  p.nombre AS producto_nombre,
+  p.imagen,
+  p.id,
+  p.descripcion AS producto_descripcion,
+  s.nombre_publico AS nombre_sede,
+  s.direccion,
+  s.telefono,
+  sc.nombre_sub 
+FROM
+  producto p 
+  INNER JOIN almacen a 
+    ON a.producto = p.id 
+  INNER JOIN sedes s 
+    ON a.sede = s.id 
+  INNER JOIN categorias c 
+    ON c.id = p.categoria 
+  INNER JOIN subcategorias sc 
+    ON sc.categoria_id = sc.id 
+WHERE p.nombre REGEXP '[[:<:]]{$keyword}*'
+  OR p.descripcion REGEXP '[[:<:]]{$keyword}*' 
+  OR c.nombre REGEXP '[[:<:]]{$keyword}*' 
+  OR sc.nombre_sub REGEXP '[[:<:]]{$keyword}*'"));
+		
 		$numPro = count($producto);
 
 
