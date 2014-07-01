d4bf4f1d0bd2e7c7d77b0543937c7c2483823f61
lizcanojair <lizcanojair@gmail.com>
2014-07-01 02:42:44 -0500

    Ventana modal para la edición de productos
    Validando ventana que permite la edición rápida de los productos.

--- app/views/empresa/editar-productos.blade.php
+++ app/views/empresa/editar-productos.blade.php
@@ -11,7 +11,30 @@
 		</h1>
 		<ul>
 			@foreach($productos as $producto)
-				<li>{{$producto->id}}. {{$producto->producto_nombre}} | <a href = '#'>Editar</a> | <a href = '#'>Eliminar</a></li>
+				<li>{{$producto->id}}. {{$producto->producto_nombre}} | <a href="#" class="btn btn-info" data-toggle="modal" data-target="#myModal-{{$producto->id}}" onclick = 'return toCloneInitialize({{$producto->id}});'><i class="fa fa-edit"></i></a>
+													 | <a href = '#'>Eliminar</a></li>
+				<div class="modal fade" id="myModal-{{$producto->id}}" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+					<div class="modal-dialog">
+					  <div class="modal-content">
+						<div class="modal-header">
+						  <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+						  <h4 class="modal-title" id="myModalLabel">Editar el producto {{$producto->producto_nombre}}</h4>
+						</div>
+						<div class="modal-body">
+						  
+						  <form class="" method="post" action="">
+							<h1>
+								Elementos disponibles para la edición
+							</h1>
+						  </form>
+						</div>
+						<div class="modal-footer">
+						  <button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
+						  
+						</div>
+					  </div>
+					</div>
+				</div>
 			@endforeach
 		</ul>
 	</div>
