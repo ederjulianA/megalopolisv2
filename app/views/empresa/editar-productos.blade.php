091ad78b949093bbc905c30652281b6e8e3159e8
lizcanojair <lizcanojair@gmail.com>
2014-07-01 02:50:17 -0500

    Formulario para la edición de productos
    Validando el ingreso de nuevos datos para el producto desde el formato
    de formulario de creación.

--- app/views/empresa/editar-productos.blade.php
+++ app/views/empresa/editar-productos.blade.php
@@ -22,11 +22,118 @@
 						</div>
 						<div class="modal-body">
 						  
-						  <form class="" method="post" action="">
-							<h1>
-								Elementos disponibles para la edición
-							</h1>
-						  </form>
+							 <form class="form-horizontal form-border" action="../editar-producto" method="post" enctype='multipart/form-data'>
+								<div class="panel-body">
+									<div class="form-group">
+										<label class="control-label col-md-2">Registrar producto en la siguiente sede:</label>
+											<div class="col-md-4">
+											<select name = 'sede' id = 'sede' class="form-control">
+												<option>Seleccionar sede:</option>
+											</select>
+										</div><!-- /.col -->
+										<div class="col-md-6">													
+										</div><!-- /.col -->
+									</div><!-- /form-group -->
+								
+									<div class="form-group">
+										<label class="control-label col-md-2">Nombre:</label>												
+										<div class="col-md-10">
+											<input id = 'product_name' name = 'product_name' type="text" class="form-control input-sm" placeholder="Nombre del Producto" value="">
+										</div><!-- /.col -->
+									</div><!-- /form-group -->
+									
+									<div class="form-group">
+										<label class="control-label col-md-2">Categoría del producto:</label>
+											<div class="col-md-4 selects-categoria">
+											<select name = 'category' id = 'category' class="form-control">
+												<option>Seleccionar categoría:</option>
+											</select>
+											<select name="subcat" id="subcat">
+												<option value="0">Escoge una </option>
+												
+											</select>
+
+											<div class="mensaje-ajax-categorias">
+												
+											</div>
+										</div><!-- /.col -->
+										<div class="col-md-6">													
+										</div><!-- /.col -->
+									</div><!-- /form-group -->
+									
+									<div class="form-group">
+										<label class="control-label col-md-2">Precio:</label>												
+										<div class="col-md-10">
+											<input id = 'product_price' name = 'product_price' type="text" class="form-control input-sm" placeholder="Precio del artículo para la sede" value="">
+										</div><!-- /.col -->
+									</div><!-- /form-group -->
+
+									<div class="form-group">
+										<label class="control-label col-md-2">Cantidad a registrar del producto en la sede seleccionada:</label>												
+										<div class="col-md-10">
+											<input id = 'product_amount' name = 'product_amount' type="text" class="form-control input-sm" placeholder="Cantidad entera del producto" value="">
+										</div><!-- /.col -->
+									</div><!-- /form-group -->
+									
+									<div class="form-group">
+										<label class="control-label col-md-2">Imagen del producto:</label>												
+										<div class="col-md-10">
+											<input id = 'imagen' name = 'imagen' type="file" class="upload-demo"  >
+										</div><!-- /.col -->
+										<div id="preview1" style="height:150px; width:auto; margin:0 auto;">
+
+											<img src=""/ style="width:auto; height:150px; ">
+											
+										</div>
+									</div><!-- /form-group -->
+
+									<div class="form-group">
+										<label class="control-label col-md-2">2da imagen:</label>												
+										<div class="col-md-10">
+											<input id = 'imagen2' name = 'imagen2' type="file" class="upload-demo"  >
+										</div><!-- /.col -->
+										<div id="preview2" style="height:150px; width:auto; margin:0 auto;">
+
+											<img src=""/ style="width:auto; height:150px; ">
+											
+										</div>
+									</div><!-- /form-group -->
+
+
+									<div class="form-group">
+										<label class="control-label col-md-2">3ra imagen:</label>												
+										<div class="col-md-10">
+											<input id = 'imagen3' name = 'imagen3' type="file" class="upload-demo"  >
+										</div><!-- /.col -->
+										<div id="preview3" style="height:150px; width:auto; margin:0 auto;">
+
+											<img src=""/ style="width:auto; height:150px; ">
+											
+										</div>
+									</div><!-- /form-group -->
+									
+									<div class="form-group">
+										<label class="control-label col-md-2">Descripción breve:</label>
+										<div class="col-md-10">
+											<textarea id = 'description' name = 'description' class="form-control" rows="3"></textarea>
+										</div><!-- /.col -->
+									</div><!-- /form-group -->
+										<div class="form-group">
+												
+												<label class="col-lg-2 control-label">Tags</label>
+												<div class="col-lg-10">
+													<input id = 'tags' type="text" class="tag-demo1" name="tags" value="">
+												</div><!-- /.col -->
+										</div><!-- /form-group -->
+
+									<div class="text-right">
+										<input type="hidden" name="empresa_id" value="{{$user->empresa->id}}">
+										<button class="btn btn-info quick-btn btn-sombra" type="submit">
+										<i class="fa fa-plus-circle"></i>Editar</button>
+										
+									</div>
+								</div>
+							</form>
 						</div>
 						<div class="modal-footer">
 						  <button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
