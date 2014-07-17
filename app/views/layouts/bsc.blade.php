270fb03481264f8e0c0032a5136f3f7c354d4169
ederjulianA <ederalvarez2091057@gmail.com>
2014-07-17 11:43:37 -0500

    editando landing BSC(2)

--- app/views/layouts/bsc.blade.php
+++ app/views/layouts/bsc.blade.php
@@ -41,8 +41,8 @@
 						<a href="#">EUR</a>-->
 					</div>
 					<div class="span2">
-						<a href="cart.html"><h4>Mis favoritos (3)</h4></a>
-						<a href="cart.html">2 item(s) - $40.00</a>
+						<a href="#"><h4>Mis favoritos (3)</h4></a>
+						<a href="#">2 item(s) - $40.00</a>
 					</div>					
 					<div class="span3 customer_service">
 						<h4>Entregas en toda la ciudad</h4>
@@ -77,14 +77,14 @@
 						<div class="nav-collapse">
 						  <ul class="nav">
 							  <li class="dropdown">
-							  <a href="category.html" class="dropdown-toggle" data-toggle="dropdown">Desktops <b class="caret"></b></a>
+							  <a href="category.html" class="dropdown-toggle" data-toggle="dropdown">Home <b class="caret"></b></a>
 							  <ul class="dropdown-menu">
-								<li><a href="listings.html">PC</a></li>
-								<li><a href="listings.html">Mac</a></li>
-								<li class="divider"></li>
+								<li><a href="listings.html">Nosotros</a></li>
+								<li><a href="listings.html">Nuestros Productos</a></li>
+								<!--<li class="divider"></li>
 								<li class="nav-header">Accessories</li>
 								<li><a href="listings.html">Keyboard</a></li>
-								<li><a href="listings.html">Speakers</a></li>
+								<li><a href="listings.html">Speakers</a></li>-->
 							  </ul>
 							</li>
 							<li><a href="category.html">Laptops</a></li>
@@ -159,137 +159,8 @@
 		<li><a href="category.html">Cameras (2)</a></li>
 	</ul>
 </div><!-- end sidebar -->		</div>
-        <div class="span9">
-
-			<div id="myCarousel" class="carousel slide">
-            <div class="carousel-inner">
-              <div class="item active">
-		<img src="BSC/css/images/carousel_1.jpg" alt="">
-                <div class="carousel-caption">
-                  <h4>First Thumbnail label</h4>
-                  <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-                </div>
-
-              </div>
-              <div class="item">
-                <img src="BSC/css/images/carousel_2.jpg" alt="">
-                <div class="carousel-caption">
-                  <h4>Second Thumbnail label</h4>
-                  <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-                </div>
-              </div>
-
-              <div class="item">
-		<img src="BSC/css/images/carousel_3.jpg" alt="">
-                <div class="carousel-caption">
-                  <h4>Third Thumbnail label</h4>
-                  <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-                </div>
-              </div>
-            </div>
-
-            <a class="left carousel-control" href="#myCarousel" data-slide="prev">&lsaquo;</a>
-            <a class="right carousel-control" href="#myCarousel" data-slide="next">&rsaquo;</a>
-          </div>
-          </div>
-		  
-		  
-		  
-		<div class="span7 popular_products">
-		 <h4>Popular products</h4><br />
-		<ul class="thumbnails">
-       
-	   <li class="span2">
-          <div class="thumbnail">
-            <a href="product.html"><img alt="" src="BSC/css/images/ps-vita-150cx123.jpg" /></a>
-            <div class="caption">
-              <a href="product.html"> <h5>PS Vita</h5></a>  Price: &#36;50.00<br /><br />
-            </div>
-          </div>
-        </li>
-
-        <li class="span2">
-          <div class="thumbnail">
-            <a href="product.html"><img alt="" src="BSC/css/images/ps-vita-150cx123.jpg" /></a>
-            <div class="caption">
-              <a href="product.html"> <h5>PS Vita</h5></a>  Price: &#36;50.00<br /><br />
-            </div>
-          </div>
-        </li>
-
-        <li class="span2">
-          <div class="thumbnail">
-            <a href="product.html"><img alt="" src="BSC/css/images/ps-vita-150cx123.jpg" /></a>
-            <div class="caption">
-              <a href="product.html"> <h5>PS Vita</h5></a>  Price: &#36;50.00<br /><br />
-            </div>
-          </div>
-        </li>
-       
-	   <li class="span2">
-          <div class="thumbnail">
-            <a href="product.html"><img alt="" src="BSC/css/images/nexus-one-3-150x123.jpg" /></a>
-            <div class="caption">
-              <a href="product.html"> <h5>Nexus one</h5></a>  Price: &#36;50.00<br /><br />
-            </div>
-          </div>
-        </li>
-       
-	   <li class="span2">
-          <div class="thumbnail">
-            <a href="product.html"><img alt="" src="BSC/css/images/thumb_sam_3d.jpg" /></a>
-            <div class="caption">
-              <a href="product.html"> <h5>Samsung 3D TV</h5></a>  Price: &#36;50.00<br /><br />
-            </div>
-          </div>
-        </li>
-       
-	   <li class="span2">
-          <div class="thumbnail">
-            <a href="product.html"><img alt="" src="BSC/css/images/ipad_case.jpg" /></a>
-            <div class="caption">
-              <a href="product.html"> <h5>iPod Case</h5></a>  Price: &#36;50.00<br /><br />
-            </div>
-          </div>
-        </li>
-       
-	   <li class="span2">
-          <div class="thumbnail">
-            <a href="product.html"><img alt="" src="BSC/css/images/HMX-H104.JPG" /></a>
-            <div class="caption">
-              <a href="product.html"> <h5>HMX Camcorder</h5></a>  Price: &#36;50.00<br /><br />
-            </div>
-          </div>
-        </li>
-       
-	   <li class="span2">
-          <div class="thumbnail">
-            <a href="product.html"><img alt="" src="BSC/css/images/expic.png" /></a>
-            <div class="caption">
-              <a href="product.html"> <h5>Kindle Fire</h5></a>  Price: &#36;50.00<br /><br />
-            </div>
-          </div>
-        </li>
-
-      </ul>
-		</div>
-        <div class="span2">
-		
-		 <div class="roe">
-		<h4>Novedades</h4><br />
-		<p>Suscribete al boletin semanal de megalopolis para estar al tanto de todas las novedades.</p>
-		
-		    <form class="form-search">
-    <input type="text" class="span2" placeholder="Tu Email" /><br /><br />
-    <button type="submit" class="btn pull-right">Suscribir</button>
-    </form>
-		</div><br /><br />
-            <a href="#"><img alt="" title="" src="BSC/css/images/paypal_mc_visa_amex_disc_150x139.gif" /></a>
-			<a href="#"><img alt="" src="BSC/css/images/bnr_nowAccepting_150x60.gif" /></a>
-
-		</div>
-	  
-      </div><footer>
+        @yield('content')
+      <footer>
 	<hr />
 	<div class="row well no_margin_left">
 
@@ -332,7 +203,7 @@
 </div> <!-- /container -->
 
 
-<div id="theme_switcher">
+<!--<div id="theme_switcher">
 	<div class="btn-group">
 		<a class="btn btn-success dropdown-toggle" data-toggle="dropdown" href="#">Switch theme <span class="caret"></span></a>
 		<ul class="dropdown-menu">
@@ -347,7 +218,7 @@
 			<li><a href="#" rel="">Bootstrap</a></li>
 		</ul>
 	</div>
-</div>
+</div>-->
 
 
 <!-- Le javascript
--- app/views/navegar.blade.php
+++ app/views/navegar.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.mega')
+@extends('layouts.bsc')
 
 
 @section('titulo')
@@ -14,439 +14,92 @@ Empresas Megalopolis ||
 <meta property="og:image" content="http://www.tumegalopolis.com/public/img/logo_2x.png"/>
 @stop
 
+@section('content')
+
 <style type="text/css">
-	.img-empresa-2{
-		height: 170px;
-		width: auto!important;
+	.img-empresa{
+		height: 110px;
+		width: auto;
 	}
 </style>
+				<div class="span9">
 
-@section('content')
-	<section id="slider"><!--slider-->
-		<div class="container">
-			<div class="row">
-			  <div class="slider_r" id="slider_r">
-			  		<div class="fluid_container">
-			  			
-			  				<div class="camera_wrap camera_azure_skin" id="camera_wrap_1">
-            <div data-thumb="SLIDE/images/slides/thumbs/slide1.jpg" data-src="SLIDE/images/slides/slide1.png">
-                <div class="camera_caption fadeFromBottom">
-                    Perfumes Megalopolis <em>Megalopolis Company</em>
+			<div id="myCarousel" class="carousel slide">
+            <div class="carousel-inner">
+              <div class="item active">
+		<img src="BSC/css/images/carousel_1.jpg" alt="">
+                <div class="carousel-caption">
+                  <h4>First Thumbnail label</h4>
+                  <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
                 </div>
-            </div>
-            <div data-thumb="SLIDE/images/slides/thumbs/leaf.jpg" data-src="SLIDE/images/slides/leaf.jpg">
-                <div class="camera_caption fadeFromBottom">
-                    Perfumes Megalopolis <em>Megalopolis Company</em>
-                </div>
-            </div>
-            <div data-thumb="SLIDE/images/slides/thumbs/road.jpg" data-src="SLIDE/images/slides/road.jpg">
-                <div class="camera_caption fadeFromBottom">
-                    Perfumes Megalopolis <em>Megalopolis Company</em>
-                </div>
-            </div>
-          
-            
-           
-        </div><!-- #camera_wrap_1 -->
-			  		</div>
 
-			  </div>
-			  <div class="pub_2" id="pub">
-			  	<iframe width="100%" height="100%" src="//www.youtube.com/embed/FG4lE5Zqkrg" frameborder="0" allowfullscreen></iframe>
-			  </div>
-			</div>
+              </div>
+              <div class="item">
+                <img src="BSC/css/images/carousel_2.jpg" alt="">
+                <div class="carousel-caption">
+                  <h4>Second Thumbnail label</h4>
+                  <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+                </div>
+              </div>
+
+              <div class="item">
+		<img src="BSC/css/images/carousel_3.jpg" alt="">
+                <div class="carousel-caption">
+                  <h4>Third Thumbnail label</h4>
+                  <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+                </div>
+              </div>
+            </div>
+
+            <a class="left carousel-control" href="#myCarousel" data-slide="prev">&lsaquo;</a>
+            <a class="right carousel-control" href="#myCarousel" data-slide="next">&rsaquo;</a>
+          </div>
+          </div>
+		  
+		  
+		  
+		<div class="span7 popular_products">
+		 <h4>Nuestras Empresas</h4><br />
+<ul class="thumbnails">
+
+	@foreach($empresas as $empresa)
+		 <li class="span2">
+          <div class="thumbnail">
+            <a href="product.html">{{HTML::image($empresa->logo, $empresa->razon_social, array('class'=>'img-empresa'))}}</a>
+            <div class="caption">
+              <a href="#"> <h5>{{$empresa->razon_social}}</h5></a>  <br /><br />
+            </div>
+          </div>
+        </li>
+	@endforeach
+       
+	  
+
+        
+
+      </ul>
 		</div>
-	</section><!--/slider-->
-	<section>
-		<div class="container">
-			<div class="row">
-				<div class="col-sm-3">
-					<div class="left-sidebar">
-					  <div class="fon_col_cat" id="fondo_cat">
-					    <h2>Categorias</h2></div>
-						
-						<div class="panel-group category-products" id="accordian"><!--category-productsr-->
-							@foreach( $categorias as $categoria)
-								<div class="panel panel-default">
-								<div class="panel-heading">
-									<h4 class="panel-title">
-										<a data-toggle="collapse" data-parent="#accordian" href="#sportswear">
-											<span class="badge pull-right"><i class="fa fa-plus"></i></span>
-											{{$categoria->nom_sector}}</a>
-									</h4>
-								</div>
-								<div id="sportswear" class="panel-collapse collapse">
-									<div class="panel-body">
-										<ul>
-											<li><a href="#">Nike </a></li>
-											<li><a href="#">Under Armour </a></li>
-											<li><a href="#">Adidas </a></li>
-											<li><a href="#">Puma</a></li>
-											<li><a href="#">ASICS </a></li>
-										</ul>
-									</div>
-								</div>
-							</div>
-							@endforeach
-							<!--<div class="panel panel-default">
-								<div class="panel-heading">
-									<h4 class="panel-title">
-										<a data-toggle="collapse" data-parent="#accordian" href="#sportswear">
-											<span class="badge pull-right"><i class="fa fa-plus"></i></span>
-											Ropa deportiva</a>
-									</h4>
-								</div>
-								<div id="sportswear" class="panel-collapse collapse">
-									<div class="panel-body">
-										<ul>
-											<li><a href="#">Nike </a></li>
-											<li><a href="#">Under Armour </a></li>
-											<li><a href="#">Adidas </a></li>
-											<li><a href="#">Puma</a></li>
-											<li><a href="#">ASICS </a></li>
-										</ul>
-									</div>
-								</div>
-							</div>
-							<div class="panel panel-default">
-								<div class="panel-heading">
-									<h4 class="panel-title">
-										<a data-toggle="collapse" data-parent="#accordian" href="#mens">
-											<span class="badge pull-right"><i class="fa fa-plus"></i></span>
-											Ropa de hombre
-										</a>
-									</h4>
-								</div>
-								<div id="mens" class="panel-collapse collapse">
-									<div class="panel-body">
-										<ul>
-											<li><a href="#">Fendi</a></li>
-											<li><a href="#">Guess</a></li>
-											<li><a href="#">Valentino</a></li>
-											<li><a href="#">Dior</a></li>
-											<li><a href="#">Versace</a></li>
-											<li><a href="#">Armani</a></li>
-											<li><a href="#">Prada</a></li>
-											<li><a href="#">Dolce and Gabbana</a></li>
-											<li><a href="#">Chanel</a></li>
-											<li><a href="#">Gucci</a></li>
-										</ul>
-									</div>
-								</div>
-							</div>
-							
-							<div class="panel panel-default">
-								<div class="panel-heading">
-									<h4 class="panel-title">
-										<a data-toggle="collapse" data-parent="#accordian" href="#womens">
-											<span class="badge pull-right"><i class="fa fa-plus"></i></span>Ropa de mujer</a>
-									</h4>
-								</div>
-								<div id="womens" class="panel-collapse collapse">
-									<div class="panel-body">
-										<ul>
-											<li><a href="#">Fendi</a></li>
-											<li><a href="#">Guess</a></li>
-											<li><a href="#">Valentino</a></li>
-											<li><a href="#">Dior</a></li>
-											<li><a href="#">Versace</a></li>
-										</ul>
-									</div>
-								</div>
-							</div>-->
-							
-						</div><!--/category-products-->
-					
-						<div class="brands_products"><!--brands_products-->
-							<div class="fon_col_cat" id="fondo_cat">
-							  <h2>Marcas</h2></div>
-							<div class="brands-name">
-								<ul class="nav nav-pills nav-stacked">
-									<li><a href="#"> <span class="pull-right">(50)</span>Nike</a></li>
-									<li><a href="#"> <span class="pull-right">(56)</span>Adidas</a></li>
-									<li><a href="#"> <span class="pull-right">(27)</span>armani</a></li>
-									<li><a href="#"> <span class="pull-right">(32)</span>pronto</a></li>
-									<li><a href="#"> <span class="pull-right">(5)</span>etnies</a></li>
-									<li><a href="#"> <span class="pull-right">(9)</span>clamp</a></li>
-									<li><a href="#"> <span class="pull-right">(4)</span>op</a></li>
-								</ul>
-							</div>
-						</div><!--/brands_products-->
-						
-						<div class="price-range"><!--price-range--></div><!--/price-range--><!--/shipping-->
-					
-					</div>
-				</div>
-				
-				<div class="col-sm-9 padding-right"><!--features_items-->
-                
-                
-               <div class="features_items"><!--features_items-->
+        <div class="span2">
+		
+		 <div class="roe">
+		<h4>Novedades</h4><br />
+		<p>Suscribete al boletin semanal de megalopolis para estar al tanto de todas las novedades.</p>
+		
+		    <form class="form-search">
+    <input type="text" class="span2" placeholder="Tu Email" /><br /><br />
+    <button type="submit" class="btn pull-right">Suscribir</button>
+    </form>
+		</div><br /><br />
+            <a href="#"><img alt="" title="" src="BSC/css/images/paypal_mc_visa_amex_disc_150x139.gif" /></a>
+			<a href="#"><img alt="" src="BSC/css/images/bnr_nowAccepting_150x60.gif" /></a>
 
-               			@foreach($empresas as $empresa)
-               				<div class="col-sm-4">
-							<div class="product-image-wrapper">
-								<div class="single-products">
-									<div class="productinfo text-center">
-										{{HTML::image($empresa->logo, $empresa->razon_social, array('class'=>'img-empresa-2'))}}
-										<h2>{{$empresa->razon_social}}</h2>
-										<p>{{$empresa->sector->nom_sector}}</p>
-										<a href="{{URL::route('empresa-info', array('nombre_publico'=>$empresa->nombre_publico))}}" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Contacto</a>
-									</div>
-									<div class="product-overlay">
-										<div class="overlay-content">
-											<h2>{{$empresa->razon_social}}</h2>
-											<p>{{$empresa->sector->nom_sector}}</p>
-											<a href="{{URL::route('empresa-info', array('nombre_publico'=>$empresa->nombre_publico))}}" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Contacto</a>
-										</div>
-									</div>
-								</div>
-								<div class="choose">
-									<ul class="nav nav-pills nav-justified">
-										<li><a href="#"><i class="fa fa-plus-square"></i>Favoritos</a></li>
-										<li><a href="#"><i class="fa fa-plus-square"></i>Lista de deseos</a></li>
-									</ul>
-								</div>
-							</div>
-						</div>
-               			@endforeach
-                  
-						
-						
-						
-				  </div>
-					
-					<div class="category-tab"><!--category-tab--></div>
-						<div class="tab-content">
-							<div class="tab-pane fade active in" id="tshirt" ></div>
-							
-							<div class="tab-pane fade" id="blazers" >
-								<div class="col-sm-3">
-									<div class="product-image-wrapper">
-										<div class="single-products">
-											<div class="productinfo text-center">
-												<img src="images/home/gallery4.jpg" alt="" />
-												<h2>$56</h2>
-												<p>Easy Polo Black Edition</p>
-												<a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</a>
-											</div>
-											
-										</div>
-									</div>
-								</div>
-								<div class="col-sm-3">
-									<div class="product-image-wrapper">
-										<div class="single-products">
-											<div class="productinfo text-center">
-												<img src="images/home/gallery3.jpg" alt="" />
-												<h2>$56</h2>
-												<p>Easy Polo Black Edition</p>
-												<a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</a>
-											</div>
-											
-										</div>
-									</div>
-								</div>
-								<div class="col-sm-3">
-									<div class="product-image-wrapper">
-										<div class="single-products">
-											<div class="productinfo text-center">
-												<img src="images/home/gallery2.jpg" alt="" />
-												<h2>$56</h2>
-												<p>Easy Polo Black Edition</p>
-												<a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</a>
-											</div>
-											
-										</div>
-									</div>
-								</div>
-								<div class="col-sm-3">
-									<div class="product-image-wrapper">
-										<div class="single-products">
-											<div class="productinfo text-center">
-												<img src="images/home/gallery1.jpg" alt="" />
-												<h2>$56</h2>
-												<p>Easy Polo Black Edition</p>
-												<a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</a>
-											</div>
-											
-										</div>
-									</div>
-								</div>
-							</div>
-							
-							<div class="tab-pane fade" id="sunglass" >
-								<div class="col-sm-3">
-									<div class="product-image-wrapper">
-										<div class="single-products">
-											<div class="productinfo text-center">
-												<img src="images/home/gallery3.jpg" alt="" />
-												<h2>$56</h2>
-												<p>Easy Polo Black Edition</p>
-												<a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</a>
-											</div>
-											
-										</div>
-									</div>
-								</div>
-								<div class="col-sm-3">
-									<div class="product-image-wrapper">
-										<div class="single-products">
-											<div class="productinfo text-center">
-												<img src="images/home/gallery4.jpg" alt="" />
-												<h2>$56</h2>
-												<p>Easy Polo Black Edition</p>
-												<a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</a>
-											</div>
-											
-										</div>
-									</div>
-								</div>
-								<div class="col-sm-3">
-									<div class="product-image-wrapper">
-										<div class="single-products">
-											<div class="productinfo text-center">
-												<img src="images/home/gallery1.jpg" alt="" />
-												<h2>$56</h2>
-												<p>Easy Polo Black Edition</p>
-												<a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</a>
-											</div>
-											
-										</div>
-									</div>
-								</div>
-								<div class="col-sm-3">
-									<div class="product-image-wrapper">
-										<div class="single-products">
-											<div class="productinfo text-center">
-												<img src="images/home/gallery2.jpg" alt="" />
-												<h2>$56</h2>
-												<p>Easy Polo Black Edition</p>
-												<a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</a>
-											</div>
-											
-										</div>
-									</div>
-								</div>
-							</div>
-							
-							<div class="tab-pane fade" id="kids" >
-								<div class="col-sm-3">
-									<div class="product-image-wrapper">
-										<div class="single-products">
-											<div class="productinfo text-center">
-												<img src="images/home/gallery1.jpg" alt="" />
-												<h2>$56</h2>
-												<p>Easy Polo Black Edition</p>
-												<a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</a>
-											</div>
-											
-										</div>
-									</div>
-								</div>
-								<div class="col-sm-3">
-									<div class="product-image-wrapper">
-										<div class="single-products">
-											<div class="productinfo text-center">
-												<img src="images/home/gallery2.jpg" alt="" />
-												<h2>$56</h2>
-												<p>Easy Polo Black Edition</p>
-												<a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</a>
-											</div>
-											
-										</div>
-									</div>
-								</div>
-								<div class="col-sm-3">
-									<div class="product-image-wrapper">
-										<div class="single-products">
-											<div class="productinfo text-center">
-												<img src="images/home/gallery3.jpg" alt="" />
-												<h2>$56</h2>
-												<p>Easy Polo Black Edition</p>
-												<a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</a>
-											</div>
-											
-										</div>
-									</div>
-								</div>
-								<div class="col-sm-3">
-									<div class="product-image-wrapper">
-										<div class="single-products">
-											<div class="productinfo text-center">
-												<img src="images/home/gallery4.jpg" alt="" />
-												<h2>$56</h2>
-												<p>Easy Polo Black Edition</p>
-												<a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</a>
-											</div>
-											
-										</div>
-									</div>
-								</div>
-							</div>
-							
-							<div class="tab-pane fade" id="poloshirt" >
-								<div class="col-sm-3">
-									<div class="product-image-wrapper">
-										<div class="single-products">
-											<div class="productinfo text-center">
-												<img src="images/home/gallery2.jpg" alt="" />
-												<h2>$56</h2>
-												<p>Easy Polo Black Edition</p>
-												<a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</a>
-											</div>
-											
-										</div>
-									</div>
-								</div>
-								<div class="col-sm-3">
-									<div class="product-image-wrapper">
-										<div class="single-products">
-											<div class="productinfo text-center">
-												<img src="images/home/gallery4.jpg" alt="" />
-												<h2>$56</h2>
-												<p>Easy Polo Black Edition</p>
-												<a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</a>
-											</div>
-											
-										</div>
-									</div>
-								</div>
-								<div class="col-sm-3">
-									<div class="product-image-wrapper">
-										<div class="single-products">
-											<div class="productinfo text-center">
-												<img src="images/home/gallery3.jpg" alt="" />
-												<h2>$56</h2>
-												<p>Easy Polo Black Edition</p>
-												<a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</a>
-											</div>
-											
-										</div>
-									</div>
-								</div>
-								<div class="col-sm-3">
-									<div class="product-image-wrapper">
-										<div class="single-products">
-											<div class="productinfo text-center">
-												<img src="images/home/gallery1.jpg" alt="" />
-												<h2>$56</h2>
-												<p>Easy Polo Black Edition</p>
-												<a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</a>
-											</div>
-											
-										</div>
-									</div>
-								</div>
-							</div>
-						</div>
-			  </div><!--/category-tab--><!--/recommended_items-->
-					
-				</div>
-			</div>
 		</div>
-	</section>
+	  
+      </div>
+
 @stop
+
+
 
 
 
