80460bd3512a88b38d534c902545f6bdadcd5a53
Carlos Andrés <cvcarlosandres39@gmail.com>
2014-08-11 12:27:55 -0500

    Arreglo de URLs
    Aún quedan pendientes por arreglar

--- app/views/layouts/tshop.blade.php
+++ app/views/layouts/tshop.blade.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+ <!DOCTYPE html>
 <html lang="en">
 <head>
 <meta charset="utf-8">
@@ -358,14 +358,6 @@
         <li class="dropdown megamenu-fullwidth"> <a data-toggle="dropdown" class="dropdown-toggle" href="#"> La Megalopolis <b class="caret"> </b> </a>
           <ul class="dropdown-menu">
             <li class="megamenu-content ">
-              <ul class="col-lg-3  col-sm-3 col-md-3 unstyled noMarginLeft newCollectionUl">
-                <li class="no-border">
-                  <p class="promo-1"> <strong> LAS EMPRESAS </strong> </p>
-                </li>
-                <li> <a href="category.html"> JIM SPORTS </a> </li>
-                <li> <a href="category.html"> DULCESITOS </a> </li>
-                <li> <a href="category.html"> MOTOS </a> </li>
-              </ul>
               
               <ul class="col-lg-3  col-sm-3 col-md-3  col-xs-4">
                 <li> <a class="newProductMenuBlock" href="#">{{HTML::image('Tshop/images/site/sport-wear.jpg', 'Producto', array('class'=>'img-responsive'))}}  <span class="ProductMenuCaption"> <i class="fa fa-caret-right"> </i> ROPA Y ACCESORIOS </span> </a> </li>
@@ -375,6 +367,9 @@
               </ul>
               <ul class="col-lg-3  col-sm-3 col-md-3 col-xs-4">
                 <li> <a class="newProductMenuBlock" href="#">{{HTML::image('Tshop/images/site/interiores.jpg', 'Interiores', array('class'=>'img-responsive'))}}  <span class="ProductMenuCaption"> <i class="fa fa-caret-right"> </i> INTERIORES </span> </a> </li>
+              </ul>
+              <ul class="col-lg-3  col-sm-3 col-md-3 col-xs-4">
+                <li> <a class="newProductMenuBlock" href="#">{{HTML::image('Tshop/images/site/tech2.jpg', 'Interiores', array('class'=>'img-responsive'))}}  <span class="ProductMenuCaption"> <i class="fa fa-caret-right"> </i> TECNOLOGÍA </span> </a> </li>
               </ul>
             </li>
           </ul>
@@ -480,16 +475,16 @@
           <h3> Soporte </h3>
           <ul>
             <li class="supportLi">
-              <p> contactanos </p>
-              <h4> <a class="inline" href="callto:+8801680531352"> <strong> <i class="fa fa-phone"> </i> 88 01680 531352 </strong> </a> </h4>
-              <h4> <a class="inline" href="mailto:help@tshopweb.com"> <i class="fa fa-envelope-o"> </i> soporte@tumegalopolis.com </a> </h4>
+              <p> Contáctanos </p>
+              <h4> <a class="inline" href="callto:+6481166"> <strong> <i class="fa fa-phone"> </i> 6481166 </strong> </a> </h4>
+              <h4> <a class="inline" href="mailto:help@tshopweb.com"> <i class="fa fa-envelope-o"> </i> megalopolis.web@gmail.com </a> </h4>
             </li>
           </ul>
         </div>
         <div class="col-lg-2  col-md-2 col-sm-4 col-xs-6">
           <h3> Compras </h3>
           <ul>
-            <li> <a href="index.html"> Home. </a> </li>
+            <li> <a href="{{URL::route('index')}}"> Home. </a> </li>
             <li> <a href="category.html"> Categorias </a> </li>
             <li> <a href="sub-category.html"> Sub Categorias </a> </li>
             <li> <a href="product-details.html"> Detalles de producto </a> </li>
@@ -510,8 +505,11 @@
         <div class="col-lg-2  col-md-2 col-sm-4 col-xs-6">
           <h3> Mi cuenta </h3>
           <ul>
-            <li> <a href="{{URL::route('login')}}">  Login </a> </li>
-            
+            <li> <a href="account-1.html"> Account Login </a> </li>
+            <li> <a href="account.html"> My Account </a> </li>
+            <li> <a href="my-address.html"> My Address </a> </li>
+            <li> <a href="wishlist.html"> Wisth list </a> </li>
+            <li> <a href="order-list.html"> Order list </a> </li>
           </ul>
         </div>
         <div class="col-lg-3  col-md-3 col-sm-6 col-xs-12 ">
@@ -520,16 +518,16 @@
             <li>
               <div class="input-append newsLatterBox text-center">
                 <input type="text" class="full text-center"  placeholder="Email ">
-                <button class="btn  bg-gray" type="button"> Suscribete/<i class="fa fa-long-arrow-right"> </i> </button>
+                <button class="btn  bg-gray" type="button"> Subscribete/<i class="fa fa-long-arrow-right"> </i> </button>
               </div>
             </li>
           </ul>
           <ul class="social">
-            <li> <a href="https://www.facebook.com/pages/TuMegalopolis/726858077327682" target="_blank"> <i class=" fa fa-facebook"> &nbsp; </i> </a> </li>
-            <li> <a href="https://twitter.com/tumegalopolis"target="_blank"> <i class="fa fa-twitter" > &nbsp; </i> </a> </li>
-           <!-- <li> <a href="https://plus.google.com"> <i class="fa fa-google-plus"> &nbsp; </i> </a> </li>
+            <li> <a href="http://facebook.com"> <i class=" fa fa-facebook"> &nbsp; </i> </a> </li>
+            <li> <a href="http://twitter.com"> <i class="fa fa-twitter"> &nbsp; </i> </a> </li>
+            <li> <a href="https://plus.google.com"> <i class="fa fa-google-plus"> &nbsp; </i> </a> </li>
             <li> <a href="http://youtube.com"> <i class="fa fa-pinterest"> &nbsp; </i> </a> </li>
-            <li> <a href="http://youtube.com"> <i class="fa fa-youtube"> &nbsp; </i> </a> </li>-->
+            <li> <a href="http://youtube.com"> <i class="fa fa-youtube"> &nbsp; </i> </a> </li>
           </ul>
         </div>
       </div>
