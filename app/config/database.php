d24d5397c52cc901a7bffe16bb37e42a22ee1ca3
ederjulianA <ederalvarez2091057@gmail.com>
2014-11-21 15:00:58 -0500

    Primer commit desde el entorno de ubuntu, revisar archivos de conexion a base de datos

--- app/config/database.php
+++ app/config/database.php
@@ -58,7 +58,7 @@ return array(
 			'host'      => 'localhost',
 			'database'  => 'megalopolis',
 			'username'  => 'root',
-			'password'  => '',
+			'password'  => 'linux4EVER',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
